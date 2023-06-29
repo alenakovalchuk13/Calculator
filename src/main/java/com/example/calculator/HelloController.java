f762530a2666f85a2de0913efe23c4b3e843d4cd
Alena Kovalchuk <alenakovalchuk13@gmail.com>
2023-06-29 18:54:34 +0300

    calculator commit

--- src/main/java/com/example/calculator/HelloController.java
+++ src/main/java/com/example/calculator/HelloController.java
@@ -1,14 +1,42 @@
 package com.example.calculator;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    private StringBuilder outPutExpression;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField expression;
+
+
+    public void insertNumber(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        outPutExpression = new StringBuilder(expression.getText());
+        outPutExpression.append(button.getText());
+        expression.setText(outPutExpression.toString());
+    }
+
+    public void calculate(ActionEvent actionEvent) {
+        Button button = (Button) actionEvent.getSource();
+        outPutExpression.append(button.getText());
+        expression.setText(outPutExpression.toString());
+
+    }
+
+    public void clear(ActionEvent actionEvent) {
+        if (outPutExpression.length() > 0)
+        {
+            outPutExpression.delete(0, outPutExpression.length());
+            expression.setText(" ");
+        }
+    }
+
+    public void back(ActionEvent actionEvent) {
+        if (outPutExpression.length() > 0)
+        {
+            outPutExpression.delete(outPutExpression.length() - 1, outPutExpression.length());
+            expression.setText(outPutExpression.toString());
+        }
     }
 }
